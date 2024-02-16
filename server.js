1c5f7b3232114292a89f8fb4b56d4529b9464a63
P SAIKUMAR <118534731+Mr-zerozero7@users.noreply.github.com>
2024-02-16 14:29:00 +0530

    time Out incresed

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/api/transactions', async (request, response)=>{
             {$limit: perPage},
             // {$skip: page * perPage},
         ]
-        const transaction = await Transaction.aggregate(pipeline);
+        const transaction = await Transaction.aggregate(pipeline).option({ maxTimeMS: 60000 });
         
         response.status(200).json({transaction});
 
@@ -109,7 +109,7 @@ app.get('/api/statistics', async (request,response)=>{
                     totalAmount: {$sum: '$price'},
                 },
             },
-        ]);
+        ]).option({ maxTimeMS: 60000 });
 
         //  TOTAL SOLD ITEMS
         const totalSoldItems = await Transaction.countDocuments({
@@ -195,7 +195,7 @@ app.get('/api/bar-chart', async (request, response) =>{
             }
         ];
     
-        const result = await Transaction.aggregate(pipeline);
+        const result = await Transaction.aggregate(pipeline).option({ maxTimeMS: 60000 });
         response.status(200).json({result});
 
     } catch (error) {
